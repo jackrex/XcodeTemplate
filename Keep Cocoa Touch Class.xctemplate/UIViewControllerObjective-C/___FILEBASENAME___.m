6545c58767b1f550fbf6b51fec41d398fff24b1a
jackrex <xiepapeng@gotokeep.com>
2017-11-02 20:01:21 +0800

    init

---
+++ Keep Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#paragm - Getter and Setter
+
+
+@end
